fb4e6c906425e5b09fd2c1d0116ed96e8cc9916d
Hsaraujo-98 <hsaraujo98@outlook.com>
2025-11-27 14:01:37 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/sql_schema_database_admin_jpa.sql
@@ -0,0 +1,59 @@
+
+CREATE TABLE Professor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeCompleto TEXT,
+    matricula TEXT,
+    email TEXT
+);
+
+CREATE TABLE Aluno (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeCompleto TEXT,
+    matricula TEXT,
+    nascimento DATE,
+    email TEXT
+);
+
+CREATE TABLE Endereco (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    logradouro TEXT,
+    endereco TEXT,
+    numero TEXT,
+    bairro TEXT,
+    cidade TEXT,
+    estado TEXT,
+    cep INTEGER,
+    aluno_id INTEGER,
+    FOREIGN KEY(aluno_id) REFERENCES Aluno(id)
+);
+
+CREATE TABLE Telefone (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    DDD TEXT,
+    numero TEXT,
+    aluno_id INTEGER,
+    FOREIGN KEY(aluno_id) REFERENCES Aluno(id)
+);
+
+CREATE TABLE MaterialCurso (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    url TEXT
+);
+
+CREATE TABLE Curso (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    sigla TEXT,
+    professor_id INTEGER,
+    material_id INTEGER,
+    FOREIGN KEY(professor_id) REFERENCES Professor(id),
+    FOREIGN KEY(material_id) REFERENCES MaterialCurso(id)
+);
+
+CREATE TABLE curso_aluno (
+    curso_id INTEGER,
+    aluno_id INTEGER,
+    PRIMARY KEY(curso_id, aluno_id),
+    FOREIGN KEY(curso_id) REFERENCES Curso(id),
+    FOREIGN KEY(aluno_id) REFERENCES Aluno(id)
+);
